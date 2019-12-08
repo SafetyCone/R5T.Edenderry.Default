1b6d9bbcdf083da1c02a90a41c0d10224e5315bb
MinexAutomation <minex.automation@gmail.com>
2019-12-08 15:03:53 -0800

    Added ServiceA implementations ServiceA1 and ServiceA2.

---
+++ source/R5T.Edenderry.Default/Code/Services/Classes/ServiceA1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace R5T.Edenderry.Default
+{
+    public class ServiceA1 : IServiceA
+    {
+        public const string ValueA1 = "A1";
+
+
+        public string GetA()
+        {
+            return ServiceA1.ValueA1;
+        }
+    }
+}
---
+++ source/R5T.Edenderry.Default/Code/Services/Classes/ServiceA2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace R5T.Edenderry.Default
+{
+    public class ServiceA2 : IServiceA
+    {
+        public const string ValueA2 = "A2";
+
+
+        public string GetA()
+        {
+            return ServiceA2.ValueA2;
+        }
+    }
+}
