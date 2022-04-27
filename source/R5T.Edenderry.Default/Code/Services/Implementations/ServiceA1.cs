fb3645e41bf2f244d8cb682371925b8efc238d02
davidcoats <david@getsafetycone.com>
2022-04-27 15:30:46 -0700

    Interim changes.

---
+++ source/R5T.Edenderry.Default/Code/Services/Implementations/ServiceA1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using R5T.T0064;
+
+
+namespace R5T.Edenderry.Default
+{
+    [ServiceImplementationMarker]
+    public class ServiceA1 : IServiceA, IServiceImplementation
+    {
+        public const string ValueA1 = "A1";
+
+
+        public string GetA()
+        {
+            return ServiceA1.ValueA1;
+        }
+    }
+}
---
+++ source/R5T.Edenderry.Default/Code/Services/Implementations/ServiceA2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using R5T.T0064;
+
+
+namespace R5T.Edenderry.Default
+{
+    [ServiceImplementationMarker]
+    public class ServiceA2 : IServiceA, IServiceImplementation
+    {
+        public const string ValueA2 = "A2";
+
+
+        public string GetA()
+        {
+            return ServiceA2.ValueA2;
+        }
+    }
+}
